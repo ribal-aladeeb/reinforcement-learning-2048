c26aba019e2615f8ee29c6f451e18c6011f353ee
jonathan mongeau <jonthemango@gmail.com>
2020-11-02 19:23:36 -0500

    Added basic updown algorithm and merge score

--- board.py
+++ board.py
@@ -13,6 +13,8 @@ class Board2048:
         self._empty_spot_numbers: List[int] = [2, 4]
         self._populate_empty_cell()
         self._populate_empty_cell()
+        self._score = 0
+        self._action_list = []
 
     def __repr__(self):
         return str(self.state)
@@ -49,21 +51,17 @@ class Board2048:
 
             if vector[current] == 0:
                 # we know that there is a non_zero value at an index further than current
-                #print(current, vector, non_zero_indices[0], vector[current])
                 vector[current] += vector[non_zero_indices[0]]
                 vector[non_zero_indices[0]] = 0
-
             elif vector[current] == vector[non_zero_indices[0]]:
                 # we know that there is a non_zero value at an index further than current
-                #print(current, vector, non_zero_indices[0], vector[current])
                 vector[current] += vector[non_zero_indices[0]]
+                self._score += vector[current]
                 vector[non_zero_indices[0]] = 0
                 current += 1
-
             elif current + 1 == non_zero_indices[0]:
                 current += 1
                 continue
-
             else:
                 # you have a non-zero entry at vector[current] and you have a non-zero entry somewhere further than current, but you don't know how many 0 are in between
                 vector[current+1] = vector[non_zero_indices[0]]
@@ -73,6 +71,7 @@ class Board2048:
         return vector
 
     def up(self):
+        self._action_list.append("up")
         result_matrix = np.apply_along_axis(self._apply_action_to_vector, axis=1, arr=self.state.T).T
         if not np.equal(result_matrix,self.state).all():
             self.state = result_matrix
@@ -80,6 +79,7 @@ class Board2048:
         return self
 
     def down(self):
+        self._action_list.append("down")
         result_matrix = np.apply_along_axis(lambda v: self._apply_action_to_vector(self._reverse_vector(v)), axis=1, arr=self.state.T)
         result_matrix = np.apply_along_axis(self._reverse_vector, axis=1, arr=result_matrix).T
         if not np.equal(result_matrix,self.state).all():
@@ -88,6 +88,7 @@ class Board2048:
         return self
 
     def left(self):
+        self._action_list.append("left")
         result_matrix = np.apply_along_axis(self._apply_action_to_vector, axis=1, arr=self.state)
         if not np.equal(result_matrix,self.state).all():
             self.state = result_matrix
@@ -95,6 +96,7 @@ class Board2048:
         return self
 
     def right(self):
+        self._action_list.append("right")
         result_matrix = np.apply_along_axis(lambda v: self._apply_action_to_vector(self._reverse_vector(v)), axis=1, arr=self.state)
         result_matrix = np.apply_along_axis(self._reverse_vector, axis=1, arr=result_matrix)
         if not np.equal(result_matrix,self.state).all():
@@ -113,17 +115,44 @@ class Board2048:
     def _simple_score(self):
         return self.state.flatten().sum(axis=0)
     
-    def show(self):
-        print(f"Simple Score: {board._simple_score()}")
-        print(self)
+    def _merge_score(self):
+        return self._score
+    
+    def show(self, ignore_zeros=False):
+        print(f"Simple Score: {self._simple_score()}")
+        print(f"Merge Score: {self._merge_score()}")
+        if ignore_zeros:
+            print(self.__repr__().replace("0.", "_."))
+        else:
+            print(self)
 
-
+def basic_updown_algorithm():
+    board = Board2048()
+    simple_score = board._simple_score()
+    while True:
+        new_state = board.perform_action("up").state
+        board.show(ignore_zeros=True)
+        new_state = board.perform_action("left").state
+        board.show(ignore_zeros=True)
+        if simple_score == board._simple_score():
+            new_state = board.perform_action('down').state
+            board.show(ignore_zeros=True)
+            new_state = board.perform_action('right').state
+            if simple_score == board._simple_score():
+                break
+        board.show(ignore_zeros=True)
+        simple_score = board._simple_score()
+    board.show()
+    print(board._action_list)
+    return board
 
 if __name__ == "__main__":
+    # basic_updown_algorithm()
+    
     board = Board2048()
-    board.show()
+    board.show(ignore_zeros=True)
     while x:=input("What is your next move: "):
         board.perform_action(x)
-        board.show()
-    print(f"Final Score: {board._simple_score()}")
+        board.show(ignore_zeros=True)
+    print(f"Final Score: {board._merge_score()}")
     print(board)
