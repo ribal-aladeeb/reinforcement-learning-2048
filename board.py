d023724656fb8d00873269b11ba2830d7c912680
jonathan mongeau <jonthemango@gmail.com>
2020-11-02 23:08:51 -0500

    Add support for peek, test case
    Co-authored-by: ribal-aladeeb <aladeeb.r@hotmail.com>

--- board.py
+++ board.py
@@ -8,13 +8,24 @@ from numpy.core.fromnumeric import nonzero
 
 class Board2048:
 
-    def __init__(self, k: int = 4):
-        self.state: np.array = np.zeros(shape=(k, k))
+    def __init__(self, k: int = 4, populate_empty_cells=True):
+        self.state: np.array = np.zeros(shape=(k, k),dtype=int)
         self._empty_spot_numbers: List[int] = [2, 4]
-        self._populate_empty_cell()
-        self._populate_empty_cell()
-        self._score = 0
-        self._action_list = []
+        self._mergescore = 0
+        self._action_history = []
+        self.k = k
+        self.populate_empty_cells = populate_empty_cells
+        
+        if populate_empty_cells:
+            self._populate_empty_cell()
+            self._populate_empty_cell()
+    
+    def clone(self) -> Board2048:
+        board = Board2048(k=self.k, populate_empty_cells=self.populate_empty_cells)
+        board.state: np.array = np.copy(self.state)
+        board._mergescore: int = self._mergescore
+        board._action_history: List[str] = self._action_history.copy()
+        return board
 
     def __repr__(self):
         return str(self.state)
@@ -56,7 +67,7 @@ class Board2048:
             elif vector[current] == vector[non_zero_indices[0]]:
                 # we know that there is a non_zero value at an index further than current
                 vector[current] += vector[non_zero_indices[0]]
-                self._score += vector[current]
+                self._mergescore += vector[current]
                 vector[non_zero_indices[0]] = 0
                 current += 1
             elif current + 1 == non_zero_indices[0]:
@@ -69,90 +80,122 @@ class Board2048:
                 current += 1
 
         return vector
-
-    def up(self):
-        self._action_list.append("up")
-        result_matrix = np.apply_along_axis(self._apply_action_to_vector, axis=1, arr=self.state.T).T
-        if not np.equal(result_matrix,self.state).all():
-            self.state = result_matrix
-            self._populate_empty_cell()  
-        return self
-
-    def down(self):
-        self._action_list.append("down")
-        result_matrix = np.apply_along_axis(lambda v: self._apply_action_to_vector(self._reverse_vector(v)), axis=1, arr=self.state.T)
-        result_matrix = np.apply_along_axis(self._reverse_vector, axis=1, arr=result_matrix).T
-        if not np.equal(result_matrix,self.state).all():
-            self.state = result_matrix
-            self._populate_empty_cell()  
-        return self
-
-    def left(self):
-        self._action_list.append("left")
-        result_matrix = np.apply_along_axis(self._apply_action_to_vector, axis=1, arr=self.state)
-        if not np.equal(result_matrix,self.state).all():
-            self.state = result_matrix
-            self._populate_empty_cell()  
-        return self
-
-    def right(self):
-        self._action_list.append("right")
-        result_matrix = np.apply_along_axis(lambda v: self._apply_action_to_vector(self._reverse_vector(v)), axis=1, arr=self.state)
-        result_matrix = np.apply_along_axis(self._reverse_vector, axis=1, arr=result_matrix)
-        if not np.equal(result_matrix,self.state).all():
-            self.state = result_matrix
-            self._populate_empty_cell()  
-        return self
     
-    def perform_action(self, action):
+    def available_moves(self) -> Set[str]:
+        moves = ['up', 'down', 'left','right']
+        available = set()
+
+        for move in moves:
+            if not (self.state == self.peek_action(move)).all():
+                available.add(move)
+        return available
+    
+    def up(self) -> Board2048:
+        board = self.clone()
+        board._action_history.append('up')
+        result_matrix = np.apply_along_axis(board._apply_action_to_vector, axis=1, arr=board.state.T).T
+        if not np.equal(result_matrix, board.state).all():
+            board.state = result_matrix
+            board._populate_empty_cell()  
+        return board
+
+    def down(self) -> Board2048:
+        board = self.clone()
+        board._action_history.append('down')
+        result_matrix = np.apply_along_axis(lambda v: board._apply_action_to_vector(board._reverse_vector(v)), axis=1, arr=board.state.T)
+        result_matrix = np.apply_along_axis(board._reverse_vector, axis=1, arr=result_matrix).T
+        if not np.equal(result_matrix, board.state).all():
+            board.state = result_matrix
+            board._populate_empty_cell()  
+        return board
+
+
+    def left(self) -> Board2048:
+        board = self.clone()
+        board._action_history.append('left')
+        result_matrix = np.apply_along_axis(board._apply_action_to_vector, axis=1, arr=board.state)
+        if not np.equal(result_matrix, board.state).all():
+            board.state = result_matrix
+            board._populate_empty_cell()  
+        return board
+
+    def right(self) -> Board2048:
+        board = self.clone()
+        board._action_history.append('right')
+        result_matrix = np.apply_along_axis(lambda v: board._apply_action_to_vector(board._reverse_vector(v)), axis=1, arr=board.state)
+        result_matrix = np.apply_along_axis(board._reverse_vector, axis=1, arr=result_matrix)
+        if not np.equal(result_matrix,board.state).all():
+            board.state = result_matrix
+            board._populate_empty_cell()  
+        return board
+    
+    def peek_action(self, action: str) -> Board2048:
+        '''
+        Returns the would-be state of the board if you were to save the state after performing the <action> argument
+        '''
         action = action.lower()[0]
         actions = {"u":"up", "d":"down", "l":"left","r":"right"}
+        
         if action in actions:
-           move_to_perform = getattr(self, actions[action]) 
-           return move_to_perform()
+            move_to_perform = getattr(self, actions[action])
+            new_board: Board2048 = move_to_perform()
+            return new_board
         raise ValueError(f"Action: {action} is invalid.")
     
-    def _simple_score(self):
+    def simple_score(self):
         return self.state.flatten().sum(axis=0)
     
-    def _merge_score(self):
-        return self._score
+    def merge_score(self):
+        return self._mergescore
     
     def show(self, ignore_zeros=False):
-        print(f"Simple Score: {self._simple_score()}")
-        print(f"Merge Score: {self._merge_score()}")
+        print(f"Simple Score: {self.simple_score()}")
+        print(f"Merge Score: {self.merge_score()}")
         if ignore_zeros:
-            print(self.__repr__().replace("0.", "_."))
+            print(self.__repr__().replace("0", "_"))
         else:
             print(self)
 
-def basic_updown_algorithm():
-    board = Board2048()
-    simple_score = board._simple_score()
+def basic_updown_algorithm(k=4):
+    board = Board2048(k=k)
+    simple_score = board.simple_score()
     while True:
-        new_state = board.perform_action("up").state
+        board = board.peek_action("up")
         board.show(ignore_zeros=True)
-        new_state = board.perform_action("left").state
+        board = board.peek_action("left")
         board.show(ignore_zeros=True)
-        if simple_score == board._simple_score():
-            new_state = board.perform_action('down').state
+        if simple_score == board.simple_score():
+            board = board.peek_action('down')
             board.show(ignore_zeros=True)
-            new_state = board.perform_action('right').state
-            if simple_score == board._simple_score():
+            board = board.peek_action('right')
+            if simple_score == board.simple_score():
                 break
         board.show(ignore_zeros=True)
-        simple_score = board._simple_score()
+        simple_score = board.simple_score()
     board.show()
-    print(board._action_list)
+    print(board._action_history)
     return board
 
 if __name__ == "__main__":
-    # basic_updown_algorithm()
+    merge_scores = []
+    maxes = []
+    simple_scores = []
+    for i in range(100):
+        board = basic_updown_algorithm(k=4)
+        board.show(ignore_zeros=True)
+        maxes.append(np.log2(np.max(board.state))) 
+        merge_scores.append(board.merge_score())
+        simple_scores.append(board.simple_score())
+
+    print(np.mean(merge_scores))
+    print(np.mean(maxes), f"{2**np.mean(maxes)}")
+    print(np.mean(simple_scores))
+    
     
     board = Board2048()
     board.show(ignore_zeros=True)
     while x:=input("What is your next move: "):
-        board.perform_action(x)
+        board = board.peek_action(x)
         board.show(ignore_zeros=True)
     print(f"Final Score: {board._merge_score()}")
     print(board)
--- tests/test_game_board.py
+++ tests/test_game_board.py
@@ -25,3 +25,36 @@ def test_game_board():
         result = board._apply_action_to_vector(np.array(example[0]))
         print(example[0], result, example[1])
         np.array_equal(result, example[1]) | grappa.should.be.true
+
+
+def test_available_moves():
+    board = Board2048(k=4, populate_empty_cells=False)
+    
+    pairs = [
+        (np.array([
+            [2,4,8,0],
+            [0,0,0,0],
+            [2,4,16,32],
+            [0,0,0,0],
+        ]), {'up','down','right'}),
+        (np.array([
+            [2,4,2,4],
+            [2,4,2,4],
+            [2,4,2,4],
+            [2,4,2,4],
+        ]), {'up','down',}),
+        (np.array([
+            [2,4,2,4],
+            [4,2,4,2],
+            [2,4,2,4],
+            [4,2,4,2],
+        ]), {} ),
+    ]
+
+    for configuration, possible_moves in pairs:
+        board.state = configuration
+        print(f'for configuration:\n')
+        print(configuration)
+        print(f'\nthe func returns {boad.available_moves()} instead of {possible_moves}')
+        # (board.available_moves() == possible_moves) | grappa.should.be.true
+        
